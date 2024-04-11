9b1e8906e486e4f9b0dcdeb6913b6eefb3644866
zach-vanzura <143541611+zach-vanzura@users.noreply.github.com>
2024-04-11 10:27:06 -0400

    added comments to sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,8 +1,10 @@
 a, b = 0, 1
 sum_even = 0
 while b < 4000000:
+    # check if even
     if b % 2 == 0:
         sum_even += b
+    # a becomes b, b becomes sum of a and b
     a, b = b, a+b
 
 print(sum_even)
